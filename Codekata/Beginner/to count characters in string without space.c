eb63da750ec9a83b96f3359032d52a5a2d98bca3
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-02-13 20:20:50 +0530

    Create to count characters in string without space.c

---
+++ Codekata/Beginner/to count characters in string without space.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  char a[90];
+  int i,letter=0,space=0;
+  scanf("%[^\n]s",a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i]>=97&&a[i]<=122||a[i]>=65&&a[i]<=90||a[i]>=33&&a[i]<=64)
+    {
+      letter++;
+    }
+    else if(a[i]==' ')
+    {
+      space++;
+    }
+  }
+  printf("%d",letter);
+}
