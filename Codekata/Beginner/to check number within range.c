f9b1007f28e69ef3ee5bc5c0d56598860fe0cd46
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 20:56:57 +0530

    Create to check number within range.c

---
+++ Codekata/Beginner/to check number within range.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,a,b,n;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&n);
+    if(n>a&&n<=b)
+    {
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+
+
+
+}
