2e62416900763d97d29c08c9704c246679efa0c0
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 18:51:06 +0530

    Create to print the string given no.of times.c

---
+++ Codekata/Beginner/to print the string given no.of times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    char a[1000];
+    scanf("%s",a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+
+        printf("\n%s",a);
+    }
+}
