f4a1eb284118b8f62f9e416086a890ec42a26c45
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 19:25:00 +0530

    Create givena number and print its digits in same order with space.c

---
+++ Codekata/Beginner/givena number and print its digits in same order with space.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[100],n,i,k=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        i=n%10;
+        a[k]=i;
+        k++;
+        //printf("%d\t",a);
+        n=n/10;
+
+    }
+    for(k=k-1;k>=0;k--)
+    {
+
+        printf("%d\t",a[k]);
+    }
+
+}
