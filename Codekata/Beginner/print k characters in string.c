c48a0b847bf97dec80c816ee94d5fb8cb14970a7
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 20:07:58 +0530

    Create print k characters in string.c

---
+++ Codekata/Beginner/print k characters in string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char a[100];
+   int i,n;
+   scanf("%s",a);
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+      printf("%c",a[i]);
+   }
+}
