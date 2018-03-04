c78ccb53d2a5a995e82d06f18756205d25e28832
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 19:07:51 +0530

    Create to find min and max in an array.c

---
+++ Codekata/Beginner/to find min and max in an array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[1000],i,temp,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+
+        for(j=i+1;j<n;j++)
+        {
+           if(a[i]>a[j])
+           {
+
+               temp=a[i];
+               a[i]=a[j];
+               a[j]=temp;
+           }
+
+
+        }
+    }
+
+
+        printf("%d\t%d",a[0],a[n-1]);
+
+
+}
