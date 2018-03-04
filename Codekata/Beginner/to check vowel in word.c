422ca841717b62bc7c35e01a18c2b9a09f3ca687
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 20:45:41 +0530

    Create to check vowel in word.c

---
+++ Codekata/Beginner/to check vowel in word.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char a[1000];
+   int i,cnt=0;
+   scanf("%s",a);
+   for(i=0;a[i]!='\0';i++)
+   {
+       if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+       {
+           cnt++;
+       }
+   }
+   if(cnt>0)
+   {
+       printf("yes");
+   }
+   else{
+    printf("no");
+   }
+}
