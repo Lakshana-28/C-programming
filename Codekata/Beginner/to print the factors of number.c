6b7978441d389639e6c716734d7f1adc123420c1
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-04 21:02:23 +0530

    Create to print the factors of number.c

---
+++ Codekata/Beginner/to print the factors of number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i,n;
+   scanf("%d",&n);
+   for(i=1;i<=100;i++)
+   {
+       if(n%i==0)
+       {
+           printf("%d\t",i);
+       }
+   }
+}
