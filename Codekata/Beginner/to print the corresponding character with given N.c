74d0c477983043eec8018b76e2a3bc36c2111f6f
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-03-13 21:55:47 +0530

    Create to print the corresponding character with given N.c

---
+++ Codekata/Beginner/to print the corresponding character with given N.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int n;
+    scanf("%d",&n);
+    printf("%c",a[n-1]);
+}
